9a7fcc3a182e543a09e62613dacce921b672a752
webinformat <webinformat@users.noreply.github.com>
2023-05-01 19:43:14 +0300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,11 +3,8 @@ basic.showIcon(IconNames.Heart)
 basic.forever(function () {
     basic.showNumber(loop)
     TPBot.setWheels(40, 80)
-    basic.pause(2000)
+    basic.pause(4800)
     TPBot.setWheels(80, 40)
-    basic.pause(4000)
-    TPBot.setTravelTime(TPBot.DriveDirection.Forward, 100, 0.2)
-    TPBot.setWheels(40, 80)
-    basic.pause(2000)
+    basic.pause(3500)
     loop += 1
 })
